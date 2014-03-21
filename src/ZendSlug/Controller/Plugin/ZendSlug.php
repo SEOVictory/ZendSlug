8beedbd98165dd4e18bf316cb30bda20aaadb5b1
vic <git@dfhu.org>
2014-03-21 19:21:38 -0400

    more whitespace issues

--- src/ZendSlug/Controller/Plugin/ZendSlug.php
+++ src/ZendSlug/Controller/Plugin/ZendSlug.php
@@ -14,68 +14,68 @@ use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 
 class ZendSlug extends AbstractPlugin
 {
-    private $_separator;
+  private $_separator;
 
-    public function getSeparator ()
-    {
-        return $this->_separator;
-    }
+  public function getSeparator ()
+  {
+    return $this->_separator;
+  }
 
-    public function setSeparator ($_replacement)
-    {
-        $this->_separator = $_replacement;
-    }
+  public function setSeparator ($_replacement)
+  {
+    $this->_separator = $_replacement;
+  }
 
-    public function __construct($config = array())
-    {
-        $this->setSeparator($config['separator']);
-    }
+  public function __construct($config = array())
+  {
+    $this->setSeparator($config['separator']);
+  }
 
-    protected function _checkReplacement($replacement = null)
-    {
-        return $replacement == NULL ? $this->getSeparator() : $replacement;
-    }
+  protected function _checkReplacement($replacement = null)
+  {
+    return $replacement == NULL ? $this->getSeparator() : $replacement;
+  }
 
-    protected function _checkTitle($title)
-    {
-        if (!isset($title))
-        {
-            throw new \Exception('Field title is required.');
-        }
-    }
+  protected function _checkTitle($title)
+  {
+    if (!isset($title))
+      {
+        throw new \Exception('Field title is required.');
+      }
+  }
 
-    protected function _url($string, $separator)
-    {
+  protected function _url($string, $separator)
+  {
     $q_separator = preg_quote($separator, '#');
     $code = array(
       '&.+?;'               => '',
       '[^a-z0-9 _-]'          => '',
       '\s+'               => $separator,
       '('.$q_separator.')+'   => $separator
-    );
+                  );
 
     $string = strip_tags($string);
     foreach ($code as $key => $val)
-    {
-      $string = preg_replace('#'.$key.'#i', $val, $string);
-    }
+      {
+        $string = preg_replace('#'.$key.'#i', $val, $string);
+      }
 
     $string = strtolower($string);
     return trim($string);
-    }
+  }
 
-    protected function _create($data)
-    {
-        $replace = isset($data['separator']) &&
-          $data['separator'] != '' ? $data['separator'] : null;
-        $separator = $this->_checkReplacement($replace);
-        $this->_checkTitle($data['title']);
-        return $this->_url($data['title'], $separator);
-    }
+  protected function _create($data)
+  {
+    $replace = isset($data['separator']) &&
+      $data['separator'] != '' ? $data['separator'] : null;
+    $separator = $this->_checkReplacement($replace);
+    $this->_checkTitle($data['title']);
+    return $this->_url($data['title'], $separator);
+  }
 
-    public function create($data = array())
-    {
-        $slug = $this->_create($data);
-        echo $slug;
-    }
+  public function create($data = array())
+  {
+    $slug = $this->_create($data);
+    echo $slug;
+  }
 }
